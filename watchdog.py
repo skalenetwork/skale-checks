eaf55aa94f20cc1587a2c8acb0b7f871c5a65b6e
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-09-29 19:02:40 +0300

    SKALE-4455 Add routes to WatchdogServer

--- watchdog.py
+++ watchdog.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import requests
 
 
@@ -19,6 +21,45 @@ class WatchdogServer:
         self.ip = node_ip
         self.timeout = timeout
         self.watchdog_url = get_watchdog_url(node_ip)
+
+    def core_status(self):
+        return self.__send_request('/status/core')
+
+    def schains_status(self):
+        return self.__send_request('/status/schains')
+
+    def sgx_status(self):
+        return self.__send_request('/status/sgx')
+
+    def hardware_status(self):
+        return self.__send_request('/status/hardware')
+
+    def endpoint_status(self):
+        return self.__send_request('/status/endpoint')
+
+    def schain_containers_versions_status(self):
+        return self.__send_request('/status/schain-containers-versions')
+
+    def meta_status(self):
+        return self.__send_request('/status/meta')
+
+    def btrfs_status(self):
+        return self.__send_request('/status/btrfs')
+
+    def ssl_status(self):
+        return self.__send_request('/status/ssl')
+
+    def ima_status(self):
+        return self.__send_request('/status/ima')
+
+    def public_ip(self):
+        return self.__send_request('/status/public-ip')
+
+    def validator_nodes(self):
+        return self.__send_request('/status/validator-nodes')
+
+    def check_report(self):
+        return self.__send_request('/status/check-report')
 
     def __send_request(self, path, raw_json=False):
         try:
