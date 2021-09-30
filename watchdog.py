2f720b621e5bc1b5e19af15d554847e10d8ae0f5
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-09-30 13:41:46 +0300

    SKALE-4455 Move server to separate class

--- watchdog.py
+++ watchdog.py
@@ -1,7 +1,4 @@
-from functools import wraps
-
-import requests
-
+from server import Server
 
 WATCHDOG_TIMEOUT_DEFAULT = 10
 WATCHDOG_PORT = 3009
@@ -16,63 +13,54 @@ class WatchdogClient:
         self.watchdog = WatchdogServer(node_ip, timeout)
 
 
-class WatchdogServer:
+class WatchdogServer(Server):
     def __init__(self, node_ip, timeout=WATCHDOG_TIMEOUT_DEFAULT):
-        self.ip = node_ip
-        self.timeout = timeout
         self.watchdog_url = get_watchdog_url(node_ip)
+        self.timeout = timeout
+        super().__init__(self.watchdog_url, self.timeout)
 
     def core_status(self):
-        return self.__send_request('/status/core')
+        return super().send_request('/status/core')
 
     def schains_status(self):
-        return self.__send_request('/status/schains')
+        return super().send_request('/status/schains')
 
     def sgx_status(self):
-        return self.__send_request('/status/sgx')
+        return super().send_request('/status/sgx')
 
     def hardware_status(self):
-        return self.__send_request('/status/hardware')
+        return super().send_request('/status/hardware')
 
     def endpoint_status(self):
-        return self.__send_request('/status/endpoint')
+        return super().send_request('/status/endpoint')
 
     def schain_containers_versions_status(self):
-        return self.__send_request('/status/schain-containers-versions')
+        return super().send_request('/status/schain-containers-versions')
 
     def meta_status(self):
-        return self.__send_request('/status/meta')
+        return super().send_request('/status/meta')
 
     def btrfs_status(self):
-        return self.__send_request('/status/btrfs')
+        return super().send_request('/status/btrfs')
 
     def ssl_status(self):
-        return self.__send_request('/status/ssl')
+        return super().send_request('/status/ssl')
 
     def ima_status(self):
-        return self.__send_request('/status/ima')
+        return super().send_request('/status/ima')
 
     def public_ip(self):
-        return self.__send_request('/status/public-ip')
+        return super().send_request('/status/public-ip')
 
     def validator_nodes(self):
-        return self.__send_request('/status/validator-nodes')
+        return super().send_request('/status/validator-nodes')
 
     def check_report(self):
-        return self.__send_request('/status/check-report')
-
-    def __send_request(self, path, raw_json=False):
-        try:
-            url = f'{self.watchdog_url}{path}'
-            response = requests.get(url=url, timeout=self.timeout)
-            response.raise_for_status()
-            raw_data = response.json()
-            if raw_json:
-                return raw_data
-            return raw_data['data']
-        except requests.exceptions.RequestException as e:
-            raise WatchdogConnectionError(e)
+        return super().send_request('/status/check-report')
 
 
 def get_watchdog_url(node_ip):
     return f'http://{node_ip}:{WATCHDOG_PORT}'
+
+
+print(WatchdogServer('18.221.96.190').sgx_status())
