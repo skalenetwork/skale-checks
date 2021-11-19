b0993c1d934b9ff7b13611c25e51af811a802aaa
Alex Sheverdin <alex@skalelabs.com>
2021-11-19 20:15:21 +0400

    Fix web3.py multithreading issue

--- skale_checks/checks/base.py
+++ skale_checks/checks/base.py
@@ -22,6 +22,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import wraps, partial
 from skale_checks.checks.types import ChecksDict, CheckStatus, Func, CheckRunners
 from skale_checks.checks.utils import get_requirements
+from importlib import reload
+from web3._utils import request
 
 
 def check(result_headers) -> Func:
@@ -31,6 +33,7 @@ def check(result_headers) -> Func:
 
         @wraps(checker)
         def wrapper(*args, **kwargs) -> ChecksDict:
+            reload(request)
             results = checker(*args, **kwargs)
             if not isinstance(results, tuple):
                 results = [results]
