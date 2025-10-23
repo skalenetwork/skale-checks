8e1e065fd70592acb2efd7cf2ef03ca8cc8c5923
Alex Sheverdin <alex@skalelabs.com>
2025-10-23 19:53:12 +0100

    Update `python_requires`

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "elasticsearch>=7.17.6,<8",  # Latest 7.x, compatible with Python 3.13 and ES 7.x
         "PyYAML==6.0.3"
     ],
-    python_requires='>=3.12,<4',
+    python_requires='>=3.13,<4',
     extras_require=extras_require,
     keywords=['skale', 'checks'],
     packages=find_packages(),
