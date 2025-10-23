9b2afa536eb72b5325715eed6ce651391397d230
Alex Sheverdin <alex@skalelabs.com>
2025-10-23 20:19:34 +0100

    Fix formatting

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
-        'Programming Language :: Python ::  3.12',
+        'Programming Language :: Python :: 3.12',
     ]
 )
